2e94b84d3ed67c591056606b41cad9b9f270f4e3
Almar Klein <almar.klein@gmail.com>
2014-07-01 09:32:32 +0200

    ak_shader_combining: Works now, but not quite happy yet
    Going to refactor again, but want to save :)

--- ak_shader_combing.py
+++ ak_shader_combing.py
@@ -1,4 +1,9 @@
 
+CALL_TEMPATE = '// CALL:'
+
+from collections import OrderedDict
+
+
 class Function(object):
     """ Simple function object for composing GLSL snippets
     
@@ -35,24 +40,83 @@ class Function(object):
       a post-hook and a pre-hook or something.
     
     """
-    def __init__(self, code, dependencies=None, inargs=None):
-        self._code = code
+    def __init__(self, code, name=None, dependencies=None):
+        self._code = code.strip()
         
-        # Try to get name. This needs to be made more robust!
-        self._name = ''
-        self._arg = ''
-        for line in code.splitlines():
-            if '(' in line:
-                self._name  = line.split(' ',1)[1].split('(')[0]
-                self._arg = line.split(' ',1)[1].split('(')[1].split(')')[0]
-        self._name = self._name.strip()
-        self._arg = self._arg.strip()
-        
-        self._inargs = inargs or []
-        self._final_code = ''
-        self._replacements = {}
+        # Stuff we need to replace code
+        self._replacements = OrderedDict()
         self._dependencies = set(dependencies or [])
+        
+        # We keep track of how we want to be called
+        self._sig = '()'
+        
+        # Prepare: get name, inject CALL_TEMPLATE, set replacements for
+        # uniforms/attributes
+        self._prepare(name)
     
+    def _prepare(self, newname=None):
+        """ Get name of function and prepare code a bit to our format.
+        """
+        # Try to get name. This needs to be made more robust!
+        name = ''
+        lines = []
+        for line in self._code.splitlines():
+            if (not name) and '(' in line:
+                name = line.split(' ',1)[1].split('(')[0].strip()
+                if newname:
+                    newname = newname.replace('{id}', hex(id(self)))
+                    line = line.replace(name, newname)
+                    name = newname
+                #self._arg = line.split(' ',1)[1].split('(')[1].split(')')[0]
+                line = line.split('//')[0].rstrip() + ' ' + CALL_TEMPATE
+                self._replacements[CALL_TEMPATE] = CALL_TEMPATE + ' ()'
+            lines.append(line)
+        # Store
+        self._name = name
+        self._code = '\n'.join(lines)
+        # Set replacements for uniforms and attributes
+        # todo: EEK! only do whole worlds, with regexp. 
+        for line in lines:
+            if line.startswith('uniform'):
+                uname = line.split(' ')[2].strip(' ;')
+                self._replacements[uname] = self.uniform(uname)
+            elif line.startswith('attribute'):
+                aname = line.split(' ')[2].strip(' ;')
+                self._replacements[aname] = self.attribute(aname)
+     
+    def _convert(self):
+        """ Apply the replacements. Return new code string.
+        """
+        code = self._code
+        for key, val in self._replacements.items():
+            code = code.replace(key, val)
+        code += '\n'
+        
+        for dep in self._dependencies:
+            code += '\n'
+            code = code + str(dep)
+        
+        return code.rstrip() + '\n'
+    
+    def __repr__(self):
+        return "<Function '%s' at %s>" % (self.name, str(id(self)))
+        
+    def __str__(self):
+        return self._convert()
+    
+    def new(self, name=None):
+        """ Make a copy of this Function object, discarting any
+        replacements and function signature. Optionally the name of the
+        function can be reset.
+        """
+        return Function(self._code, name=name)
+    
+    def apply(self, name=None):
+        """ Create a new Function object that consists of the current
+        one with all replacements applied. Optionally the name of the
+        function can be reset.
+        """
+        return Function(str(self), name=name)
     
     @property
     def name(self):
@@ -62,28 +126,36 @@ class Function(object):
     
     @property
     def call_line(self):
-        
-        #argname = '$' + self._arg.split(' ')[1]
-        #argname = '$%s_%s' % (self.name, self._arg.split(' ')[1])
-        args = []
-        for arg in self._inargs:
-            if isinstance(arg, Function):
-                args.append(arg.call_line)
-                self._dependencies.add(arg)
-            elif isinstance(arg, str):
-                args.append(arg)
-            else:
-                raise ValueError('Dont know how to inject %r' % val)
-        s = ', '.join(args)
-        return '%s(%s)' % (self.name, s)
+        """ The line of code used to call this function.
+        """
+        try:
+            sig = self._replacements[CALL_TEMPATE].split(':')[1].strip()
+        except KeyError:
+            sig = '()'
+        return self._name + sig
+    
+    def uniform(self, uname):
+        """ Get mangled uniform name given the initial name.
+        """
+        return uname + '__' + self._name
+        #return self._replacements[uname]
+    
+    def attribute(self, aname):
+        """ Get mangled attribute name given the initial name.
+        """
+        return aname + '__' + self._name
+        #return self._replacements[aname]
     
     def __setitem__(self, key, val):
+        """ Setting of replacements through a dict-like syntax. Replacements
+        can be verbatim code and Function objects.
+        """
         if key not in self._code:
             raise ValueError('Could not find %r in code of %r' % (key, self))
         assert isinstance(key, str)
         
         if isinstance(val, Function):
-            # todo: what about args?
+            self._replacements[key+'('] = val.name + '('
             self._replacements[key] = val.call_line
             self._dependencies.add(val)
         elif isinstance(val, str):
@@ -91,78 +163,86 @@ class Function(object):
         else:
             raise ValueError('Dont know how to inject %r' % val)
     
-    def __call__(self, *args):
-        self._convert()
-        fun = self
-        return Function(fun._final_code, self._dependencies, args)
+    def __call__(self, *args, **kwargs):
+        """ Set the signature for this function. Each argument can be
+        verbatim code or a Function object.
+        """
+        name = kwargs.pop('name', None)
+        if kwargs:
+            raise ValueError('Invalid arguments: %r' % list(kwargs.keys()))
+        str_args = []
+        for arg in args:
+            if isinstance(arg, Function):
+                str_args.append(arg.call_line)
+                self._dependencies.add(arg)
+            elif isinstance(arg, str):
+                str_args.append(arg)
+            else:
+                raise ValueError('Dont know how to inject %r' % val)
+        s = ', '.join(str_args)
+        self._replacements[CALL_TEMPATE] = CALL_TEMPATE + ' (%s)' % s
+        return self
+        #self._convert()
+        #fun = self
+        #return Function(fun._final_code, self._dependencies, args)
     
     
-    def _convert(self):
-        code = self._code
-        for key, val in self._replacements.items():
-            code = code.replace(key, val)
-        
-        for dep in self._dependencies:
-            code += '\n'
-            code = code + str(dep)
-            
-        self._final_code = code.strip() + '\n'
-    
-    def __repr__(self):
-        return "<Function '%s' at %s>" % (self.name, str(id(self)))
-        
-    def __str__(self):
-        self._convert()
-        return self._final_code
-    
-
-
 ##
-
-Normal = Function("""
-vec4 normal(vec4 pos)
-{
-    ... do some magic
-    return pos;
-}
-""")
 
 
 Position = Function("""
 attribute vec4 a_position;
-
 vec4 position()
 {   
     return a_position;
-{
-
+}
 """)
 
-Transform = Function("""
-vec4 transform(vec4 pos)
+TransformScale = Function("""
+uniform float u_scale;
+vec4 transform_scale(vec4 pos)
 {
-    pos.z += 1;
+    pos.xyz *= u_scale;
     return pos;
 }
 """)
 
+TransformZOffset = Function("""
+uniform float u_zoffet;
+vec4 transform_zoffset(vec4 pos)
+{
+    pos.z += u_zoffet;
+    return pos;
+}
+""")
 
 Frag_template = Function("""
 void main (void)
 {
     int nlights = $nlights;
-    gl_Position = $position;
+    vec4 pos = $position;
+    gl_Position = $endtransform(pos);
 }
 
 """)
 
 
 
-code = Frag_template()
-code['$position'] = Transform(Position())
+# Get function objects. Generate random name for transforms
+code = Frag_template.new()
+position = Position.new()
+t1 = TransformScale.new('trans'+str(1))
+t2 = TransformZOffset.new('trans'+str(2))
+t3 = TransformScale.new('trans'+str(3))
+t4 = TransformScale.new()
+
+# Compose everything together
+code['$position'] = t1(t2(t3(position())))
+code['$endtransform'] = t4()
 code['$nlights'] = '4'
 
+# Show result
 print(code)
 
-
-
+# Yay, we can easil obtain the uniform names via their original function object
+print(t1.uniform('u_scale'), t3.uniform('u_scale'))
